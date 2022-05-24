b8a7f926656a11eb3346b7d8107167524911bbbd
Ricardo Orozco <mrmkm@live.com>
2022-05-24 12:33:43 -0500

    Implementing new vue project template

---
+++ demo-vue/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Matematico :number1="10" :number2="10" operacion="/"/>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Matematico from './components/Matematico.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Matematico
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ demo-vue/src/components/Matematico.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <label>{{number1}} {{operacion}} {{number2}} = {{result}}</label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Matematico',
+  props: {
+    number1: Number,
+    number2: Number,
+    operacion: String
+  },
+  data() {
+    return {
+        result: 50
+    }
+  },
+  methods: {
+    calcular: function() {
+        if(this.operacion === '+')
+            this.result = this.number1 + this.number2
+
+        else if(this.operacion === '-')
+            this.result = this.number1 - this.number2
+
+        else if(this.operacion === '*')
+            this.result = this.number1 * this.number2
+
+        else if(this.operacion === '/')
+            this.result = this.number1 / this.number2
+    }
+  },
+  watch: {
+    $props: {
+        handler() {
+            this.calcular();
+        },
+        immediate:true
+    }
+  }
+}
+</script>
